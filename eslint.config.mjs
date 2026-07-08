a7386222a72b74977cb871824b82bf7eda61b8f6
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 15:09:59 -0600

    chore(lint): integrate eslint-config-flat-gitignore to dynamically apply .gitignore rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,16 @@
+import gitignore from "eslint-config-flat-gitignore"
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals"
 import eslintConfigPrettier from "eslint-config-prettier"
 import onlyWarn from "eslint-plugin-only-warn"
 
 const eslintConfig = [
+  gitignore(),
   ...nextCoreWebVitals,
   eslintConfigPrettier,
   {
     plugins: {
       "only-warn": onlyWarn,
     },
-  },
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "coverage/**",
-    ],
   },
 ]
 
