a1cd3ee7211f51cc8d753aa02b61f14f3eddb382
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-09 16:13:08 -0600

    chore(config): migrate codegen config to typescript

---
+++ codegen.ts
@@ -0,0 +1,21 @@
+import type { CodegenConfig } from "@graphql-codegen/cli"
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "https://graphql-pokemon2.vercel.app/",
+  documents: "**/*.tsx",
+  generates: {
+    "./graphql/generated.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-query",
+      ],
+      config: {
+        fetcher: "fetch",
+      },
+    },
+  },
+}
+
+export default config
