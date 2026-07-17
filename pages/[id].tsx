2b2f090f719d2c94ff395e5da22e968036c3fa6d
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-17 15:38:04 -0600

    refactor(images): give Pokémon artwork a focused responsive presentation owner

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,9 +1,9 @@
 import gql from "graphql-tag"
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next"
-import Image from "next/image"
 import Link from "next/link"
 import { Fragment } from "react"
 import AppContainer from "@/components/AppContainer"
+import PokemonImage from "@/components/PokemonImage"
 import { Pokemon, PokemonsQuery } from "@/graphql/generated"
 import classNames from "@/utils/classNames"
 import { fetchPokemonApi } from "@/utils/fetchPokemonApi"
@@ -301,35 +301,6 @@ function PaginationButton({
         {text}
       </div>
     </Link>
-  )
-}
-
-function PokemonImage({
-  size,
-  imageUrl,
-  altText,
-}: {
-  size: "h-8 w-8" | "h-16 w-16"
-  imageUrl: string
-  altText: string
-}) {
-  return (
-    <div
-      className={classNames(
-        "relative overflow-hidden rounded-full bg-white",
-        size === "h-16 w-16"
-          ? "h-16 w-16 border-2 border-solid border-gray-400"
-          : "h-8 w-8",
-      )}
-    >
-      <Image
-        src={imageUrl}
-        fill
-        sizes={size === "h-16 w-16" ? "64px" : "32px"}
-        alt={altText}
-        className="h-full w-full object-contain"
-      />
-    </div>
   )
 }
 
